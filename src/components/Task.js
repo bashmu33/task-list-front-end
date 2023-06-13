f4642962fc45abe8091d0c0ce68b40d9f1e2faa5
Paige <ph.paigee@gmail.com>
2023-06-13 15:04:46 -0400

    completed wave 01, changed Task param to props

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,20 +3,26 @@ import PropTypes from 'prop-types';
 
 import './Task.css';
 
-const Task = ({ id, title, isComplete }) => {
-  const buttonClass = isComplete ? 'tasks__item__toggle--completed' : '';
+// id, title, isComplete
+
+const Task = ({ props }) => {
+  const buttonClass = props.isComplete ? 'tasks__item__toggle--completed' : '';
   return (
     <li className="tasks__item">
-      <button className={`tasks__item__toggle ${buttonClass}`}>{title}</button>
+      <button className={`tasks__item__toggle ${buttonClass}`}>{props.title}</button>
       <button className="tasks__item__remove button">x</button>
     </li>
   );
 };
 
 Task.propTypes = {
+  // tasks.PropTypes.arrayOf(
+  //   PropTypes.shape({
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   isComplete: PropTypes.bool.isRequired,
+// })
+// )
 };
 
 export default Task;
