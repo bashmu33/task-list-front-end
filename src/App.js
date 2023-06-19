eb3051ce313164e5204ef1c9246dde5b5168a199
Muniba <mbashirkimes@gmail.com>
2023-06-19 13:53:23 -0400

    Added get and delete API endpoints

--- src/App.js
+++ src/App.js
@@ -16,10 +16,42 @@ const TASKS = [
   },
 ];
 
-export const URL = 'https://task-list-api-c17.onrender.com/';
+export const URL = 'https://task-list-api-c17.onrender.com/tasks';
 
 const App = () => {
   const [tasks, setTasks] = useState(TASKS);
+
+  useEffect(() => {
+    axios
+      .get(URL)
+      .then((response) => {
+        const newTasks = response.data.map((task) => {
+          return {
+            id: task.id,
+            title: task.title,
+            isComplete: task.isComplete,
+          };
+        });
+        setTasks(newTasks);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const deleteTask = (taskId) => {
+    console.log('yellow')
+    axios
+      .delete(`${URL}/${taskId}`)
+      .then(() => {
+        const newTasks = tasks.filter((task) => task.id !== taskId);
+        setTasks(newTasks);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
 
   const updateTaskCompletion = (taskId) => {
     setTasks((prevTasks) => {
@@ -29,12 +61,6 @@ const App = () => {
         }
         return task;
       });
-    });
-  };
-
-  const deleteTask = (taskId) => {
-    setTasks((prevTasks) => {
-      return prevTasks.filter(task => task.id !== taskId);
     });
   };
 
